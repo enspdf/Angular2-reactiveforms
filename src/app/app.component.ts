c21f4396823a99f01cfb3d87d349efd37f954b0e
Sebastian Higuita Castañeda <sebastianhc1995@hotmail.com>
2016-11-28 23:14:44 -0500

    simple reactive forms with angular 2 app example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Contacts } from './contacts.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    contactinfo: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+
+    }
+
+    ngOnInit() {
+        this.contactinfo = this.formBuilder.group({
+            name: ['', [Validators.required, Validators.minLength(4)]],
+            contactno: ['', [Validators.required, Validators.minLength(7)]]
+        });
+
+        //Using the FormGroup control
+        //this.contactinfo = new FormGroup({
+            //name: new FormControl('Shackox'),
+            //contactno: new FormControl('0000')
+        //});
+    }
+
+    onSubmit({value, valid}: {value: Contacts, valid: boolean}) {
+        console.log(value, valid);
+    }
 }
